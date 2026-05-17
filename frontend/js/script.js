31b75a96b256c3ea3fcc931e5b08356ddf098efc
andikas23 <andkaa.s23@gmail.com>
2026-05-18 00:23:07 +0700

    froentend ui ui

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,16 @@
 console.log("Phishing Detection System Running...");
+const detectButton = document.querySelector(".detect-btn");
+const resultBox = document.getElementById("resultBox");
+
+detectButton.addEventListener("click", () => {
+  const urlInput = document.querySelector(".url-input").value;
+
+  if (urlInput.trim() === "") {
+    alert("Please enter a URL first.");
+    return;
+  }
+
+  // nanti bagian ini connect ke Flask + ML
+
+  resultBox.classList.remove("hidden");
+});
